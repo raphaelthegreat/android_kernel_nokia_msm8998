ed7f2f7a9d0c898540615d726c7c66a801996805
Sudarshan Rajagopalan <sudaraja@codeaurora.org>
2017-05-18 00:11:06 -0700

    msm: secure_buffer: Add SPSS_SP vmid
    Add support for content protection for ION
    memory allocated to SPSS through user process.
    
    Change-Id: I1aa37717c02a5b96468d16ec1a26bb2ee7020e74
    Signed-off-by: Sudarshan Rajagopalan <sudaraja@codeaurora.org>

--- drivers/staging/android/ion/msm/msm_ion.c
+++ drivers/staging/android/ion/msm/msm_ion.c
@@ -674,6 +674,7 @@ bool is_secure_vmid_valid(int vmid)
 		vmid == VMID_CP_SEC_DISPLAY ||
 		vmid == VMID_CP_APP ||
 		vmid == VMID_CP_CAMERA_PREVIEW ||
+		vmid == VMID_CP_SPSS_SP ||
 		vmid == VMID_CP_SPSS_SP_SHARED);
 }
 
@@ -695,6 +696,8 @@ int get_secure_vmid(unsigned long flags)
 		return VMID_CP_APP;
 	if (flags & ION_FLAG_CP_CAMERA_PREVIEW)
 		return VMID_CP_CAMERA_PREVIEW;
+	if (flags & ION_FLAG_CP_SPSS_SP)
+		return VMID_CP_SPSS_SP;
 	if (flags & ION_FLAG_CP_SPSS_SP_SHARED)
 		return VMID_CP_SPSS_SP_SHARED;
 	return -EINVAL;
